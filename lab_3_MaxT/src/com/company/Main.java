645690dffc326e6ae2a90470da98f89e1bd0d4c4
Maxim-Turovets <mturovec71@gmail.com>
2018-03-01 18:23:39 +0200

    add func

--- lab_3_MaxT/src/com/company/Main.java
+++ lab_3_MaxT/src/com/company/Main.java
@@ -20,12 +20,15 @@ System.out.print("ВАШ ТЕКСТ\n\n");
             "Этот художник своей жизни явно не смог создать для себя ничего, кроме уродливого прозябания.\n" +
             "Кроншоу на него поглядел и наполнил свой стакан.\n" +
             " Он послал официанта за пачкой сигарет.\n"+
-                    "— Помилуйте, это ведь сложный вопрос. А как бы вы сами на него ответили?\n"+
-                    "— Никак, потому что ответ этот каждый должен найти для себя сам. Но для чего, по-вашему, вы родились на свет божий?\n";
+                    "— Помилуйте, это ведь сложный вопрос.А как бы вы сами на него ответили?\n"+
+                    "— Никак, потому что ответ этот каждый должен найти для себя сам.Но для чего, по-вашему, вы родились на свет божий?\n";
 
         System.out.print(str);
         System.out.print("\n\nВопросительные предложения :\n");
-        Parsing(str);
+
+        String [] string_array = Parsing(str,'?');   // нахождение вопросителных предложений
+        output(string_array);                            // вывод  вопросительных предложений
+
 
     }
 
@@ -33,9 +36,8 @@ System.out.print("ВАШ ТЕКСТ\n\n");
 
 
 
-    public static void /*String []*/ Parsing (String str)
+    public static String [] Parsing (String str,char dtr)
     {
-        char dtr='?';
         int count = 1,index=0;
 
         for (int i=0;i<str.length();i++)
@@ -58,14 +60,19 @@ System.out.print("ВАШ ТЕКСТ\n\n");
             if(str.charAt(i)=='.')
               min_str[index]="";
 
-            if(str.charAt(i)=='?')
+            if(str.charAt(i)==dtr)
             index++;
           }
 
-      //  System.out.print(min_str.length);
-          for (int i=0;i<min_str.length;i++) {
-              System.out.print(min_str[i]);
-              System.out.print("\n");
-          }
+          return min_str;
+    }
+
+    public static void  output (String [] str)
+    {
+        for (int i=0;i<str.length;i++)
+        {
+            System.out.print(str[i]);
+            System.out.print("\n");
+        }
     }
 }
